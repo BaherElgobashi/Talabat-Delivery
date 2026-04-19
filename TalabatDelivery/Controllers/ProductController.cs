0f6c6d0987b6c10f1f487f1f846eab51d9b0ec32
Baher <baher8040@gmail.com>
2026-04-19 20:00:47 +0200

    Add GetAllProducts as an Endpoint in the Product Controller.

--- TalabatDelivery/Controllers/ProductController.cs
+++ TalabatDelivery/Controllers/ProductController.cs
@@ -9,10 +9,23 @@ namespace TalabatDelivery.Controllers
     
     public class ProductController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet("{id}")]
         public ActionResult<Product> GetById(int id)
         {
             return new Product { Id = id , Name = "test" };
+        }
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Product>> GetAllProducts()
+        {
+            return new List<Product>()
+            {
+                new Product(){Id = 1, Name = "test1"},
+                new Product(){Id = 1, Name = "test2"},
+                new Product(){Id = 3, Name = "test3"},
+                new Product(){ Id = 4, Name = "test4"}
+            };
         }
     }
 }
