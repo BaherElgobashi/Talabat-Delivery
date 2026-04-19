d606c015e87e1b6a24e657e551d1aae7c3a2e216
Baher <baher8040@gmail.com>
2026-04-19 18:50:28 +0200

    Add GetById as an Endpoint inside the ProductController

--- TalabatDelivery/Controllers/ProductController.cs
+++ TalabatDelivery/Controllers/ProductController.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TalabatDelivery.Models;
 
 namespace TalabatDelivery.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    
     public class ProductController : ControllerBase
     {
-        
+        [HttpGet]
+        public ActionResult<Product> GetById(int id)
+        {
+            return new Product { Id = id , Name = "test" };
+        }
     }
 }
