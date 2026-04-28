7f532cb264913d877c6d765db5c16409fe7b0c46
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-04-28 14:43:39 +0300

    Add Product Entity inside the Products Folder in the ECommerce.Domain Projects.

---
+++ ECommerce.Domain/Entities/Products/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Entities.Products
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string PictureUrl { get; set; } = default!;
+        public decimal Price { get; set; }
+
+        #region Relationships
+
+
+
+
+        #endregion
+    }
+}
