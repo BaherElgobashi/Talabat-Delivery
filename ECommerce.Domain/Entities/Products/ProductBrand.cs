aa731fb28261e588cc2262f93b23866ab618a9ec
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-04-28 14:44:32 +0300

    Add ProductBrandEntity inside the Products Folder in the ECommerce.Domain Projects

--- ECommerce.Domain/Entities/Products/ProductBrand.cs
+++ ECommerce.Domain/Entities/Products/ProductBrand.cs
@@ -8,5 +8,7 @@ namespace ECommerce.Domain.Entities.Products
 {
     public class ProductBrand
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
     }
 }
