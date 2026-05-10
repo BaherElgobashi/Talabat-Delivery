b3d511f8cba3e35e6669836d3c27662b454ac941
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-10 14:57:44 +0300

    Add the DbContext Service inside the Program.cs file.

--- TalabatDelivery/Program.cs
+++ TalabatDelivery/Program.cs
@@ -1,4 +1,7 @@
 
+using ECommerce.Presistance.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace TalabatDelivery
 {
     public class Program
@@ -12,6 +15,12 @@ namespace TalabatDelivery
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            }
+            );
 
             var app = builder.Build();
 
