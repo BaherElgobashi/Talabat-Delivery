91a67a8008e0b6b7f227e8d4663e33d75264bc70
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-10 14:53:22 +0300

    Make the Constructor Chaining inside the StoreDbContext and Datasets for the project.

---
+++ ECommerce.Presistance/DbContexts/StoreDbContext.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Domain.Entities.Products;
+using ECommerce.Presistance.Configurations.Products_Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistance.DbContexts
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions <StoreDbContext> options) :base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // We use them because DbContext and Configurations in the same Project.
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly()); 
+
+            // we use this if congiguration and DbContext not in the same project.
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly); // Both of them are the same 
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+    }
+}
--- TalabatDelivery/Controllers/ProductController.cs
+++ TalabatDelivery/Controllers/ProductController.cs
@@ -22,7 +22,7 @@ namespace TalabatDelivery.Controllers
             return new List<Product>()
             {
                 new Product(){Id = 1, Name = "test1"},
-                new Product(){Id = 1, Name = "test2"},
+                new Product(){Id = 2, Name = "test2"},
                 new Product(){Id = 3, Name = "test3"},
                 new Product(){Id = 4, Name = "test4"}
             };
