376ef0076c4ff7363e7e8783eb7593e5da9537d9
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-16 15:05:58 +0300

    Add UnitOfWork inside the Repositories folder and make it implement the IUnitOfWork Interface.

---
+++ ECommerce.Presistance/Repositories/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistance.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IGenericRepository<TEntity, TKey> GetGenericRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
