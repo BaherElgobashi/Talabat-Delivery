5e4ba1974968ae4fe052cc72d3e63c871444c295
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-16 14:43:13 +0300

    Add Repositories Folder inside the ECommerce.Presistence Projects and add GenericRepository that implement the IGenericRepository inside it.

---
+++ ECommerce.Presistance/Repositories/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistance.Repositories
+{
+    public class GenericRepository<TEntity, Tkey> : IGenericRepository<TEntity, Tkey> where TEntity : BaseEntity<Tkey>
+    {
+        public Task<TEntity> AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(Tkey Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
