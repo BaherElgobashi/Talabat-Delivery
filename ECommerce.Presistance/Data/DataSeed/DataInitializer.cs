c26b06da36859c35e40a6ec4f5d60029da4d391d
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-16 16:08:00 +0300

    Add DataInitializer inside the DataSeed Folder and Make it Implement IDataInitializer.

---
+++ ECommerce.Presistance/Data/DataSeed/DataInitializer.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistance.Data.DataSeed
+{
+    public class DataInitializer : IDataInitializer
+    {
+        public void Intialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
