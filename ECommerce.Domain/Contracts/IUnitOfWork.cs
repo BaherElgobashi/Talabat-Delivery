3daff48d56f067e1a46610d3041e6973f45ad3ab
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-16 15:04:02 +0300

    Add GetGenericRepository inside the IUnitOfWork Interface.

--- ECommerce.Domain/Contracts/IUnitOfWork.cs
+++ ECommerce.Domain/Contracts/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECommerce.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace ECommerce.Domain.Contracts
 {
     public interface IUnitOfWork
     {
+        IGenericRepository<TEntity , TKey> GetGenericRepository<TEntity , TKey>() where TEntity : BaseEntity<TKey>;
+
         Task<int> SaveChangesAsync();
     }
 }
