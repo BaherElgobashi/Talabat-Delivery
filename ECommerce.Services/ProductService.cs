d2ee35081cc65d4b3cbc043504cad0a0cccbd60d
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-18 14:11:57 +0300

    Add ProductService and make it implement the IProductService Interface.

---
+++ ECommerce.Services/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.Services.Abstraction.Services;
+using ECommerce.Shared.DTOS.ProductDTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Services
+{
+    public class ProductService : IProductService
+    {
+        public Task<IEnumerable<BrandDTO>> GetAllBrandsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDTO>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TypeDTO>> GetAllTypesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDTO> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
