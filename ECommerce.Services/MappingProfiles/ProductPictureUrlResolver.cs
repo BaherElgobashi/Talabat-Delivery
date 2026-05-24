0eced9cb15525ad9a6ff2c57ad4208d2253d3208
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-24 15:10:38 +0300

    Make an Object from IConfiguration by using the Dependency Injection.

--- ECommerce.Services/MappingProfiles/ProductPictureUrlResolver.cs
+++ ECommerce.Services/MappingProfiles/ProductPictureUrlResolver.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using ECommerce.Domain.Entities.Products;
 using ECommerce.Shared.DTOS.ProductDTOS;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,15 @@ namespace ECommerce.Services.MappingProfiles
 {
     public class ProductPictureUrlResolver : IValueResolver<Product, ProductDTO, string>
     {
+        private readonly IConfiguration _configuration;
+
+        public ProductPictureUrlResolver(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public string Resolve(Product source, ProductDTO destination, string destMember, ResolutionContext context)
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
