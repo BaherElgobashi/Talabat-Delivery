86ad379812e01bf892b3068c11ca75c79ef86f25
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-28 15:24:19 +0300

    Add the Constructor inside the ProductWithTypeAndBrandSpecification.

--- ECommerce.Services/Specifications/ProductWithTypeAndBrandSpecification.cs
+++ ECommerce.Services/Specifications/ProductWithTypeAndBrandSpecification.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Entities.Products;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace ECommerce.Services.Specifications
 {
-    public class ProductWithTypeAndBrandSpecification : BaseSpecifications<int>
+    public class ProductWithTypeAndBrandSpecification : BaseSpecifications<Product ,int>
     {
+        public ProductWithTypeAndBrandSpecification() : base()
+        {
+            AddInclude(P => P.ProductType);
+            AddInclude(P => P.ProductBrand);
+        }
     }
 }
