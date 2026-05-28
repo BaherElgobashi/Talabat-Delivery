3c8928c21eba7c2d750b73237a5f54b24a34598e
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-28 15:22:43 +0300

    Make ProductWithTypeAndBrandSpecification inherit from BaseSpecifications.

--- ECommerce.Services/Specifications/ProductWithTypeAndBrandSpecification.cs
+++ ECommerce.Services/Specifications/ProductWithTypeAndBrandSpecification.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECommerce.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ECommerce.Services.Specifications
 {
-    public class ProductWithTypeAndBrandSpecification 
+    public class ProductWithTypeAndBrandSpecification : BaseSpecifications<int>
     {
     }
 }
