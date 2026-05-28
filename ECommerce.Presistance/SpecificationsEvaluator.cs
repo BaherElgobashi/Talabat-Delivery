cd7ffe8e6a3a1aac0c215206a53426d1a8cdc69a
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-28 15:02:18 +0300

    Add CreateTEntity as a static Function inside the SpecificationsEvaluator.

---
+++ ECommerce.Presistance/SpecificationsEvaluator.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace ECommerce.Presistance
+{
+    public static class SpecificationsEvaluator
+    {
+        public static IQueryable<TEntity> CreateTEntity<TEntity , TKey> (IQueryable<TEntity> EntryPoint ,
+            ISpecifications<TEntity, TKey> specifications) where TEntity : BaseEntity<TKey>
+        {
+            var Query = EntryPoint;
+            if (specifications is not null)
+            {
+                if (specifications.IncludeExpressions is not null && specifications.IncludeExpressions.Any())
+                {
+                    foreach (var IncludeExp in specifications.IncludeExpressions)
+                    {
+                        Query = Query.Include(IncludeExp);
+                    }
+                }
+
+            }
+            return Query;
+        }
+    }
+}
