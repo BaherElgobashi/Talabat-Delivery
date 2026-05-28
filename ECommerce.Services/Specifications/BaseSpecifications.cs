ec1e42904cf1a22e16f3d803f84ef4d8bd31c6d2
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-05-28 14:30:55 +0300

    Make BaseSpecification implememnt the ISpecification Interface.

--- ECommerce.Services/Specifications/BaseSpecifications.cs
+++ ECommerce.Services/Specifications/BaseSpecifications.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ECommerce.Services.Specifications
 {
-    public class BaseSpecifications
+    public abstract class BaseSpecifications<TEntity, TKey> : ISpecifications<TEntity, TKey> where TEntity : BaseEntity<TKey>
     {
+        public ICollection<Expression<Func<TEntity, object>>> IncludeExpressions => throw new NotImplementedException();
     }
 }
