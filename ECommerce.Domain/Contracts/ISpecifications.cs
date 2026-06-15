1aad1ba2dd973e61ec55fc4bae63671386a7fee3
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-06-15 21:50:55 +0300

    Add OrderByDescending as a new signature inside the ISpecifications.

--- ECommerce.Domain/Contracts/ISpecifications.cs
+++ ECommerce.Domain/Contracts/ISpecifications.cs
@@ -15,6 +15,7 @@ namespace ECommerce.Domain.Contracts
         public Expression<Func<TEntity, bool>> Criteria { get; }
 
         public Expression<Func<TEntity , object>> OrderBy { get; }
+        public Expression<Func<TEntity , object>> OrderByDescending { get; }
 
     }
 }
