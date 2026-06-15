862ece9c5f84aa0c306e1ca25dab720a73f5aebc
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-06-15 14:18:27 +0300

    Add new Constructor inside the ProductWithTypeAndBrandSpecification to add brandId and typeId as Parameters.

--- ECommerce.Services/Specifications/ProductWithTypeAndBrandSpecification.cs
+++ ECommerce.Services/Specifications/ProductWithTypeAndBrandSpecification.cs
@@ -18,6 +18,16 @@ namespace ECommerce.Services.Specifications
             AddInclude(P => P.ProductBrand);
         }
 
+
+        // Get All Products.
+        public ProductWithTypeAndBrandSpecification(int? brandId , int? typeId) 
+            : base(P => (!brandId.HasValue || P.BrandId == brandId)
+            && (!typeId.HasValue || P.TypeId == typeId))
+        {
+            AddInclude(P => P.ProductType);
+            AddInclude(P => P.ProductBrand);
+        }
+
         // Get Products By Id.
 
         public ProductWithTypeAndBrandSpecification(int id ) : base(P => P.Id == id) 
