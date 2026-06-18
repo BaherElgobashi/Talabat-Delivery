1bfbf2063c09822697d0053b4cdf1c232af9e19e
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-06-18 22:49:12 +0300

    Add ProductCountSpecifications inside the Specifications Folder.

---
+++ ECommerce.Services/Specifications/ProductCountSpecifications.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Services.Specifications
+{
+    public class ProductCountSpecifications : BaseSpecifications<Product, int>
+    {
+
+    }
+}
