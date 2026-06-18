af446f97c93a05329ab0de2e9892645d855d42ff
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-06-18 23:15:38 +0300

    Refactor the ProductWithTypeAndBrandSpecification to use GetProductCriteria.

--- ECommerce.Services/Specifications/ProductWithTypeAndBrandSpecification.cs
+++ ECommerce.Services/Specifications/ProductWithTypeAndBrandSpecification.cs
@@ -22,10 +22,8 @@ namespace ECommerce.Services.Specifications
 
         // Get All Products.
         public ProductWithTypeAndBrandSpecification(ProductQueryParams queryParams)
-            : base(P => (!queryParams.BrandId.HasValue || P.BrandId == queryParams.BrandId.Value)
-            && (!queryParams.TypeId.HasValue || P.TypeId == queryParams.TypeId.Value)
-            && (string.IsNullOrEmpty(queryParams.Search) || P.Name.ToLower().Contains(queryParams.Search.ToLower()))
-            )
+            : base(ProductSpecificationsHelper.GetProductCriteria(queryParams))
+            
 
         {
             AddInclude(P => P.ProductType);
