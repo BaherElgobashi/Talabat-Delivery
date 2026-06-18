53ef4484a615bfe3b9ae2f8a950656ee54c502ab
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-06-18 22:51:04 +0300

    Add ProductCountSpecifications constructor inside the ProductCountSpecifications class.

--- ECommerce.Services/Specifications/ProductCountSpecifications.cs
+++ ECommerce.Services/Specifications/ProductCountSpecifications.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerce.Domain.Entities.Products;
+using ECommerce.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,12 @@ namespace ECommerce.Services.Specifications
 {
     public class ProductCountSpecifications : BaseSpecifications<Product, int>
     {
-
+        public ProductCountSpecifications(ProductQueryParams queryParams) 
+            : base(P => (!queryParams.BrandId.HasValue || P.BrandId == queryParams.BrandId.Value)
+            && (!queryParams.TypeId.HasValue || P.TypeId == queryParams.TypeId.Value)
+            && (string.IsNullOrEmpty(queryParams.Search) || P.Name.ToLower().Contains(queryParams.Search.ToLower())))
+        {
+            
+        }
     }
 }
