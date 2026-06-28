02e316a59fca76bb3d4b7bc6e9b9e2ab9bf9c85c
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-06-28 23:20:32 +0300

    Implement the IBasketRepository Interface inside the BasktetRepository Class.

--- ECommerce.Presistance/Repositories/BasktetRepository.cs
+++ ECommerce.Presistance/Repositories/BasktetRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities.Basket_Module;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace ECommerce.Presistance.Repositories
 {
-    public class BasktetRepository
+    public class BasktetRepository : IBasketRepository
     {
+        public Task<CustomerBasket?> CreateOrUpdateAsync(CustomerBasket basket, TimeSpan timeToLive = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteBasketAsync(string basketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomerBasket?> GetBasketAsync(string basketId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
