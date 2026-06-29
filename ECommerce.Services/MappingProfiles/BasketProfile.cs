f5d5dbe5a1b5fff6dab6d9f2c7ad41acc29ad33a
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-06-30 00:19:06 +0300

    Add two mapping profiles inside the BasketProfile Constructor.

--- ECommerce.Services/MappingProfiles/BasketProfile.cs
+++ ECommerce.Services/MappingProfiles/BasketProfile.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using ECommerce.Domain.Entities.Basket_Module;
+using ECommerce.Shared.DTOS.BasketDTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,12 @@ using System.Threading.Tasks;
 
 namespace ECommerce.Services.MappingProfiles
 {
-    public class BasketProfile
+    public class BasketProfile : Profile
     {
+        public BasketProfile()
+        {
+            CreateMap<CustomerBasket, BasketDTO>().ReverseMap();
+            CreateMap<BasketItem, BasketItemDTO>().ReverseMap();
+        }
     }
 }
