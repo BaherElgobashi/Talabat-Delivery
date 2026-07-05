0f774146194e6b08d60ca8643df8d20f399e7f2b
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-07-05 21:54:00 +0300

    Implement ICacheService Interface inside the CacheService Class.

--- ECommerce.Services/CacheService.cs
+++ ECommerce.Services/CacheService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Services.Abstraction.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace ECommerce.Services
 {
-    internal class CacheService
+    public class CacheService : ICacheService
     {
+        public Task<string?> GetAsync(string CacheKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetAsync(string CacheKey, string CacheValue, TimeSpan TimeToLive)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
